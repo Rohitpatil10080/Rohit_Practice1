b633d477b21e0033b82477ce9aa245764245ba0b
Sam Smith <rohitpatil10080@gmail.com>
2024-05-07 20:52:43 +0530

    First commit

---
+++ Section12_Assignment2.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Section12_Assignment2 {
+
+	public static void main(String[] args) throws InterruptedException {
+	
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+	 driver.findElement(By.id("autocomplete")).sendKeys("ind");
+	 Thread.sleep(3000);
+	 driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+	 driver.findElement(By.id("autocomplete")).sendKeys(Keys.DOWN);
+
+	 
+	 driver.findElement(By.xpath("//div[text()='India']")).click();
+
+//	 Actions s =new Actions(driver);
+//	   
+//	s.moveToElement(driver.findElement(By.xpath("//div[text()='India']"))); 
+//	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
