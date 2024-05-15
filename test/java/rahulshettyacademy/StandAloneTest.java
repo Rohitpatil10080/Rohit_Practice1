6fa08797b3a1e1fb73ca06e073dc144c06dba1f8
Sam Smith <rohitpatil10080@gmail.com>
2024-05-15 21:35:15 +0530

    second project

--- test/java/rahulshettyacademy/StandAloneTest.java
+++ test/java/rahulshettyacademy/StandAloneTest.java
@@ -1,9 +1,17 @@
 package rahulshettyacademy;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class StandAloneTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		WebDriverManager.chromedriver().setup();
+		
+		
+	
 
 	}
 
